9495f3e9fa74981e829f1a34aab11d5670478227
Danil Mikhailov <danil@altlinux.org>
2016-08-16 15:31:24 +0300

    JS: remove js from html

--- js/dr.js
+++ js/dr.js
@@ -2,24 +2,33 @@ function getRandomInt(min, max){
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var iter = 100000;
-var daysInYear = 356;
+function getDrProbability(){
+
+var iter = 1000;
+var daysInYear = 365;
 var probability = 0;
 
 for (i = 0; i < iter; i++){
     var drs=0;
-    var days = new Array(daysInYear);
-    for (j = 0; j < 400; j++){
-	var day = getRandomInt(0, daysInYear);
-	days[day] = days[day]++;
+    var days = Array.apply(null, Array(daysInYear)).map(Number.prototype.valueOf,0);
+    for (j = 1; j < 400; j++){
+	var day = getRandomInt(1, daysInYear);
+	//console.log(day);
+	days[day] = days[day] + 1;
 	if (days[day] > 4){
 	    drs++; 
 	    days[day] = 0; //надо проверять что этот день ещё не учитывали
 	    continue;
 	}
+        //console.log(days);
     }
     probability += drs > 0 ? 1 : 0; //если был хоть один день с > 4 значит случилось, добавляем 1
     //console.log(i);
     //console.log("result");
     //console.log(drs);
+}
+
+console.log("final result");
+console.log(probability);
+console.log(probability / iter);
 }
