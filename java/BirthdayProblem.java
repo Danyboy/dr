951bcc5bef1b756e0988545d214a713ee7dc3ee4
Danil Mikhailov <danil@altlinux.org>
2016-08-18 01:14:31 +0300

    Added java

---
+++ java/BirthdayProblem.java
@@ -0,0 +1,29 @@
+class BirhdayProblem {
+
+    public static double birthdayProblem (int friends, int birthdaysInOneDay , int iteration){
+	int daysInYear = 365;
+	int result = 0;
+
+	for (int i = 0; i < iteration; i++){
+	    int [] days = new int [daysInYear];
+	    for (int j = 0; j < friends; j++){
+		//int currentDay = ;
+		//days[currentDay] = days[currentDay] + 1;
+		if (++days[myRandomInt(0, daysInYear - 1)] >= birthdaysInOneDay){
+		    result++;
+		    break;
+		}
+	    }
+	}
+	
+	return ((double) result / (double) iteration);
+    }
+    
+    public static int myRandomInt(int min, int max){
+	return (int) (min + Math.random() * (max - min));
+    }
+
+    public static void main(String args[]){
+	System.out.println(birthdayProblem(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2])));
+    } 
+}
